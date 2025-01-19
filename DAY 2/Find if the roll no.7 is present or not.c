77d6cfdd9528d85d316fdcdcab45d022c80ebbd8
Rupsha696 <ghoshrupsha321@gmail.com>
2025-01-19 13:59:37 +0530

    Create Find if the roll no.7 is present or not.c

---
+++ DAY 2/Find if the roll no.7 is present or not.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+int main() {
+    int a=7,b;
+   printf("Enter the roll no.");
+   scanf("%d",&b);
+   if(a==b)
+   printf("Roll number %d is present in the class");
+   
+   
+    return 0;
+}
