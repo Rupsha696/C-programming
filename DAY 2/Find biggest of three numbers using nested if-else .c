fc34a3c8423ccb113785470eba417231308d32b6
Rupsha696 <ghoshrupsha321@gmail.com>
2025-01-19 14:13:41 +0530

    Create Find biggest of three numbers using nested if-else .c

---
+++ DAY 2/Find biggest of three numbers using nested if-else .c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+int main() {
+   int a,b,c;
+   printf("Enter the numbers ");
+   scanf("%d %d %d",&a,&b,&c);
+   if(a>b){
+       if(a>c){
+           printf("A is biggest");
+       }
+       else{
+           printf("C is biggest");
+       }
+   }
+   else{
+       if(b>c){
+           printf("B is biggest");
+       }
+       else{
+           printf("C is biggest");
+       }
+   }
+   
+    return 0;
+}
